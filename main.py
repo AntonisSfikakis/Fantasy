b6c6ecc2b41f8934327bbade1cc42f9ce8f20fd7
AntonisSfikakis <sdi2200178@di.uoa.gr>
2026-07-21 08:54:33 +0300

    more  progress

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from euroleague_api.shot_data import ShotData
+
+# Welcome message
 def WelcomeMessage(): 
     print("Hallo! Welcome to euroluege Fantasy winner!")
-    print("Select one of 2\n A)Best lineup with my point\n B)I choose some players\n")
+    print("Select one of 2\n A)Best lineup with my point\n B)I choose some players")
 
-# read points
-def PointsMessage(points):
+# Points message 
+def PointsMessage(points: int):
     print("You currently have: ",points," left")
 
 
@@ -11,3 +14,14 @@ WelcomeMessage()
 choice = input()
 print("How many available points do you have?")
 points = input()
+
+
+season = 2025 
+game_code = 1
+competition_code = "E"
+
+shotdata = ShotData(competition_code)
+df = shotdata.get_game_shot_data(season, game_code)
+print(df)
+
+
